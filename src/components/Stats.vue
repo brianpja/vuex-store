1f6f926e9e0790965b6e0c6e8e5c34557ac910aa
Brian James <brian.james@slalom.com>
2020-06-09 11:42:32 -0700

    add link and remove link mutations and actions added

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,18 +2,50 @@
   <div class="stats">
     <h1>This is in a different Stats Component</h1>
     <p>There are currently {{ countLinks }} links</p>
+
+    <button v-on:click="removeAllLinks">Remove all links</button>
+    <p>{{msg}}</p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'Stats',
+  data() {
+    return {
+      msg: ''
+    }
+  },
   computed: {
     ...mapGetters([
       'countLinks'
-    ])
+    ]),
+
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions([
+      'removeAll'
+    ]),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'Poof! They are gone!'
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+  button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // state in the vuex store is shared across all components
   state: {
     title: 'My Custom Title',
     links: [
@@ -12,14 +13,39 @@ export default new Vuex.Store({
       'http://youtube.com'
     ]
   },
+  // getters get the data from state and allow you to perform computations on it
   getters: {
     countLinks: state => {
       return state.links.length
     }
   },
+  // mutations let you change the data in state
   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state) => {
+      state.links = [];
+    }
   },
+  // use actions for asynchronous functionality
   actions: {
+    removeLink: (context, link) => {
+      context.commit('REMOVE_LINK', link)
+    },
+    // uses object destructuring to call commit without context
+    // this example waits 1.5 seconds and then calls REMOVE_ALL mutation
+    removeAll({ commit }) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
   },
   modules: {
   }
